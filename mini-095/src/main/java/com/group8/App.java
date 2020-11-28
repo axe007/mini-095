636e538a02c12b3da625bc2cca8dd3e240d9b183
Renyuan Huang <hry87625@gmail.com>
2020-11-28 18:46:11 +0100

    update pom for javafx

--- mini-095/src/main/java/com/group8/App.java
+++ mini-095/src/main/java/com/group8/App.java
@@ -2,15 +2,25 @@ package com.group8;
 
 import com.group8.controllers.MainController;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        MainController mainController = new MainController();
-        mainController.startApplication();
+public class App extends Application {
+    public static void main(String[] args) {
+        // MainController mainController = new MainController();
+        // mainController.startApplication();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+
+        FXMLLoader listLoader = new FXMLLoader(getClass().getResource("/examples/mvp/list/list.fxml"));
     }
 }
--- mini-095/src/main/java/com/group8/controllers/LoginViewController.java
+++ mini-095/src/main/java/com/group8/controllers/LoginViewController.java
@@ -1,26 +1,42 @@
 package com.group8.controllers;
 
+import com.group8.constants.Constants;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.fxml.FXML;
 
 public class LoginViewController {
-    @FXML
-    private Label titleLabel;
-    @FXML
-    private TextField userNameTextField;
-    @FXML
-    private PasswordField passwordField;
-    @FXML
-    private Button loginButton;
+    
+    public Label titleLabel;
+    public TextField userNameTextField;
+    public PasswordField passwordField;
+    public Button loginButton;
 
-    @FXML
-    private void handleLoginButtonAction(ActionEvent event) {
+    public void handleLoginButtonAction(ActionEvent event) {
         // Button was clicked, do something...
+        String userName = userNameTextField.getText();
+        String password = passwordField.getText();
+        if(userName!= null && !userName.isEmpty()&&password!=null&&!password.isEmpty()){
+            if(userName.equalsIgnoreCase("admin")){
+                if(password.equals(Constants.ADMIN_PASSWORD)){
+                    // turn into next screen
+                }else{
+                    // display error info
+
+                }
+            }else{
+                
+
+
+            }
+
+        }else{
+
+        }
 
     }
 
