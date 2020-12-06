bdaec43c25018b07be4cfc6da8ad325282dea284
Altansukh T <altansukh@altansukh.com>
2020-12-06 18:07:57 +0800

    PMO-58 Login GUI update
    - Added authentication error message
    - Added labels
    - Needs to fix successful authentication

--- mini-095/src/main/java/com/group8/controllers/LoginViewController.java
+++ mini-095/src/main/java/com/group8/controllers/LoginViewController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class LoginViewController {
     @FXML
@@ -21,35 +22,28 @@ public class LoginViewController {
     private PasswordField passwordField;
     @FXML
     private Button loginButton;
+    @FXML
+    private HBox loginError;
 
     @FXML
     private void handleLoginButtonAction(ActionEvent event) throws IOException {
         // Button was clicked, do something...
         String userName = userNameTextField.getText();
         String password = passwordField.getText();
-        if (userName != null && !userName.isEmpty() && password != null && !password.isEmpty()) {
-            if (userName.equalsIgnoreCase(Constants.ADMIN_USERNAME)) {
-                if (password.equals(Constants.ADMIN_PASSWORD)) {
-                    // turn into next screen
-                    App.setRoot("AdminView");
 
-                } else {
-                    // display error info
-
-                }
-            } else {
-
-            }
-
+        if (userName.equalsIgnoreCase(Constants.ADMIN_USERNAME) && password.equals(Constants.ADMIN_PASSWORD)) {
+            // turn into next screen
+            App.setRoot("AdminView");
         } else {
-
+            // display error info
+            loginError.setVisible(true);
         }
-
     }
 
     @FXML
     private void handleResetButtonAction(ActionEvent event) throws IOException {
         userNameTextField.clear();
         passwordField.clear();
+        loginError.setVisible(false);
     }
 }
