725e48f34ac7980d39ced100fb6d27e9c5c2ed15
flyf <hry87625@gmail.com>
2021-01-08 12:11:55 +0100

    clean up codebase

--- mini-095/src/main/java/com/group8/model/GanttChartActivity.java
+++ mini-095/src/main/java/com/group8/model/GanttChartActivity.java
@@ -5,26 +5,12 @@ import java.time.LocalDate;
 import org.bson.types.ObjectId;
 
 public class GanttChartActivity {
-    // MARK: For trial only
     private String title;
     private LocalDate startDate;
     private LocalDate endDate;
     private ObjectId topLevelID;
     private ObjectId middleLevelID;
     private ObjectId bottomLevelID;
-    // private ArrayList<Integer> workingSchedule;
-
-    // public GanttChartActivity(String title, LocalDate startDate, LocalDate
-    // endDate, int topLevelID, int middleLevelID,
-    // int bottomLevelID, ArrayList<Integer> workingSchedule) {
-    // this.title = title;
-    // this.startDate = startDate;
-    // this.endDate = endDate;
-    // this.topLevelID = topLevelID;
-    // this.middleLevelID = middleLevelID;
-    // this.bottomLevelID = bottomLevelID;
-    // this.workingSchedule = workingSchedule;
-    // }
 
     public GanttChartActivity(String title, LocalDate startDate, LocalDate endDate, ObjectId topLevelID,
             ObjectId middleLevelID, ObjectId bottomLevelID) {
@@ -34,21 +20,7 @@ public class GanttChartActivity {
         this.topLevelID = topLevelID;
         this.middleLevelID = middleLevelID;
         this.bottomLevelID = bottomLevelID;
-        // this.workingSchedule = new ArrayList<>();
     }
-
-    // public GanttChartActivity(String title, LocalDate startDate, LocalDate
-    // endDate, int topLevelID, int middleLevelID,
-    // int bottomLevelID, LocalDate projectStartDate, LocalDate projectEndDate) {
-    // this.title = title;
-    // this.startDate = startDate;
-    // this.endDate = endDate;
-    // this.topLevelID = topLevelID;
-    // this.middleLevelID = middleLevelID;
-    // this.bottomLevelID = bottomLevelID;
-    // setWorkingSchedule(projectStartDate, projectEndDate);
-
-    // }
 
     public String getTitle() {
         return title;
@@ -97,43 +69,5 @@ public class GanttChartActivity {
     public void setBottomLevelID(ObjectId bottomLevelID) {
         this.bottomLevelID = bottomLevelID;
     }
-
-    // public ArrayList<Integer> getWorkingSchedule() {
-    // return workingSchedule;
-    // }
-
-    // public void setWorkingSchedule(ArrayList<Integer> workingSchedule) {
-    // this.workingSchedule = workingSchedule;
-    // }
-
-    // public void setWorkingSchedule(LocalDate projectStartDate, LocalDate
-    // projectEndDate) {
-    // ArrayList<Integer> temp = new ArrayList<>();
-    // // Get Start day of the project
-    // Locale locale = new Locale("SE");
-    // int currentWeek = projectStartDate.get(WeekFields.of(locale).weekOfYear()) -
-    // 1;
-    // int currentYear = projectStartDate.getYear();
-    // Calendar cal = Calendar.getInstance();
-    // cal.setWeekDate(currentYear, currentWeek, 1);
-    // //
-    // LocalDate localDate = LocalDateTime.ofInstant(cal.toInstant(),
-    // cal.getTimeZone().toZoneId()).toLocalDate();
-    // int count = 0;
-
-    // for (LocalDate i = localDate; i.isBefore(projectEndDate); i.plusDays(1)) {
-    // if (i.isEqual(projectStartDate) || i.isEqual(projectEndDate)) {
-    // temp.add(1);
-    // } else if (i.isAfter(projectStartDate) && i.isBefore(projectEndDate)) {
-    // temp.add(1);
-
-    // } else {
-    // temp.add(0);
-    // }
-    // count++;
-    // }
-    // this.workingSchedule = temp;
-    // System.out.println("Class count: " + count);
-    // }
 
 }
